9bdd8566ac37462ac430646cee26f65711b2ca73
John McCall <john@lowlydba.com>
2024-07-04 13:19:20 -0400

    fix: fix types packaging

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,4 @@
-import getICD10Description from "../src/index";
+import getICD10Description from "@lowlysre/icd-10-cm";
 
 describe("getICD10Description", () => {
   it("should return the correct description for a valid ICD-10 code", () => {
